83ba71dff105b06b42d707eba6444f2f9db305ab
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2022-03-26 18:04:22 -0300

    feat: creates methods to give required info to score

--- lib/models/pinfall.rb
+++ lib/models/pinfall.rb
@@ -14,6 +14,30 @@ class Pinfall
     validate!
   end
 
+  def total_falled_pins
+    quantities.sum
+  end
+
+  def related_total_falled_pins
+    return [quantities[0..1].sum, 10].min if three_times? # have to do a strike?
+
+    quantities.sum
+  end
+  
+  def strike?
+    quantities.size == 1 && total_falled_pins == 10 ||
+      quantities.size == 3 && quantities.first == 10
+  end
+
+  def spare?
+    quantities.size == 2 && total_falled_pins == 10 ||
+      quantities.size == 3 && quantities.first != 10
+  end
+
+  def three_times?
+    quantities.size == 3
+  end
+
   private
 
   def update_fouls
--- spec/lib/models/pinfall_spec.rb
+++ spec/lib/models/pinfall_spec.rb
@@ -3,12 +3,13 @@ require 'models/pinfall'
 require 'exceptions/invalid_record'
 
 RSpec.describe Pinfall do
-  subject { Pinfall.new(quantities) }
+  subject { pinfall }
+
+  let(:quantities) { [7, 2] }
+  let(:pinfall) { Pinfall.new(quantities) }
 
   describe 'validations' do
     context 'with right quantities' do
-      let(:quantities) { [7, 2] }
-      
       it { is_expected.to be_truthy }
 
       context 'with F as a quantity' do
@@ -65,6 +66,92 @@ RSpec.describe Pinfall do
 
         it { is_expected.to raise_error(InvalidRecord, amount_error_message) }
       end
+    end
+  end
+  
+  describe '#total_falled_pins' do
+    subject { pinfall.total_falled_pins }
+
+    it { is_expected.to eq(9) }
+  end
+
+  describe '#related_total_falled_pins' do
+    subject { pinfall.related_total_falled_pins }
+
+    it { is_expected.to eq(9) }
+
+    context 'with three times' do
+      context 'when the first time is a strike' do
+        let(:quantities) { [10, 8, 1] }
+  
+        it { is_expected.to eq(10) }
+      end
+
+      context 'with a spare' do
+        let(:quantities) { [7, 3, 1] }
+  
+        it { is_expected.to eq(10) }
+      end
+    end
+  end
+
+  describe '#strike?' do
+    subject { pinfall.strike? }
+
+    it { is_expected.to be_falsey }
+
+    context 'with only one quantity and total_falled_pins 10' do
+      let(:quantities) { [10] }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'with three times' do
+      let(:quantities) { [7, 3, 1] }
+
+      it { is_expected.to be_falsey }
+
+      context 'with the first time is 10' do
+        let(:quantities) { [10, 8, 1] }
+
+        it { is_expected.to be_truthy }
+      end
+    end
+  end
+
+  describe '#spare?' do
+    subject { pinfall.spare? }
+
+    it { is_expected.to be_falsey }
+
+    context 'when the total_falled_pins is 10' do
+      let(:quantities) { [7, 3] }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'with three times' do
+      let(:quantities) { [10, 8, 1] }
+      
+      it { is_expected.to be_falsey }
+      
+      context 'with with the first time is not 10' do
+        let(:quantities) { [7, 3, 1] }
+
+        it { is_expected.to be_truthy }
+      end
+    end
+  end
+
+  describe '#three_times?' do
+    subject { pinfall.three_times? }
+    
+    it { is_expected.to be_falsey }
+
+    context 'when the total_falled_pins is 10' do
+      let(:quantities) { [10, 8, 1] }
+
+      it { is_expected.to be_truthy }
     end
   end
 end
