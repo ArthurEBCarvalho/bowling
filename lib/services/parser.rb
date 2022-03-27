eb9bfca3f14b252bb7ca2ed173b7b39854136ea8
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2022-03-27 10:54:26 -0300

    feat: creates parser

---
+++ lib/services/parser.rb
@@ -0,0 +1,61 @@
+require 'pry'
+
+class Parser
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+  end
+
+  def call
+    skip_next = false
+    group_file.map do |name, rounds|
+      pinfalls = []
+      rounds   = rounds.map { |round| round.split("\t").last.delete("\r\n") }
+
+      populate_pinfalls(pinfalls, rounds, skip_next)
+
+      { name => pinfalls }
+    end
+  end
+
+  private
+
+  def group_file
+    File.open(file).group_by { |line| line.split("\t").first }
+  end
+
+  def populate_pinfalls(pinfalls, rounds, skip_next)
+    rounds.size.times do |index|
+      if skip_next || last_round?(pinfalls)
+        skip_next = false
+        next
+      end
+
+      skip_next = skip_next?(rounds, index, pinfalls)
+      pinfalls << round_pinfalls(rounds, index, pinfalls)
+    end
+  end
+
+  def last_round?(pinfalls)
+    pinfalls.size == 10
+  end
+
+  def round_pinfalls(rounds, index, pinfalls)
+    if last_but_one_round?(pinfalls)
+      [rounds[index], rounds[index + 1], rounds[index + 2]]
+    elsif rounds[index] == '10'
+      [rounds[index]]
+    else
+      [rounds[index], rounds[index + 1]]
+    end
+  end
+
+  def last_but_one_round?(pinfalls)
+    pinfalls.size == 9
+  end
+
+  def skip_next?(rounds, index, pinfalls)
+    !last_but_one_round?(pinfalls) && rounds[index] != '10'
+  end
+end
---
+++ spec/lib/services/parser_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'services/parser'
+
+RSpec.describe Parser do
+  subject { Parser.new(file).call }
+
+  let(:file) { file_fixture('scores.txt') }
+  let(:player_jeff) { { 'Jeff' => jeff_painfalls } }
+  let(:jeff_painfalls) { [['10'], ['7', '3'], ['9', '0'], ['10'], ['0', '8'], ['8', '2'], ['F', '6'], ['10'], ['10'], ['10', '8', '1']] }
+  let(:player_john) { { 'John' => john_painfalls } }
+  let(:john_painfalls) { [['3', '7'], ['6', '3'], ['10'], ['8', '1'], ['10'], ['10'], ['9', '0'], ['7', '3'], ['4', '4'], ['10', '9', '0']] }
+
+  describe '#call' do
+    it { is_expected.to eq([player_jeff, player_john]) }
+  end
+end
