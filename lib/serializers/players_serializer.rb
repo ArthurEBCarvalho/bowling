205556461b805d97fab8fb0b3ac085e2ae930984
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2022-03-27 21:07:04 -0300

    feat: creates players serializer

---
+++ lib/serializers/players_serializer.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class PlayersSerializer
+  attr_reader :players, :lines
+
+  def initialize(players)
+    @players = players
+    @lines   = [%w[Frame 1 2 3 4 5 6 7 8 9 10].join("\t\t")]
+  end
+
+  def call
+    players.each do |player|
+      lines << player.name
+      lines << pinfalls(player)
+      lines << score(player)
+    end
+
+    lines.join("\r\n")
+  end
+
+  private
+
+  def pinfalls(player)
+    "Pinfalls\t#{player.pinfalls.map(&:to_s).join("\t")}"
+  end
+
+  def score(player)
+    "Score\t\t#{player.scores.join("\t\t")}"
+  end
+end
---
+++ spec/lib/serializers/players_serializer_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'serializers/players_serializer'
+require 'models/player'
+require 'models/pinfall'
+
+RSpec.describe PlayersSerializer do
+  subject { PlayersSerializer.new(players).call }
+
+  let(:players) { [player_jeff, player_john] }
+  let(:player_jeff) { Player.new('Jeff', pinfalls_jeff) }
+  let(:pinfalls_jeff) do
+    [
+      Pinfall.new(['10']),
+      Pinfall.new(['7', '3']),
+      Pinfall.new(['9', '0']),
+      Pinfall.new(['10']),
+      Pinfall.new(['0', '8']),
+      Pinfall.new(['8', '2']),
+      Pinfall.new(['F', '6']),
+      Pinfall.new(['10']),
+      Pinfall.new(['10']),
+      Pinfall.new(['10', '8', '1'])
+    ]
+  end
+
+  let(:player_john) { Player.new('John', pinfalls_john) }
+  let(:pinfalls_john) do
+    [
+      Pinfall.new(['3', '7']),
+      Pinfall.new(['6', '3']),
+      Pinfall.new(['10']),
+      Pinfall.new(['8', '1']),
+      Pinfall.new(['10']),
+      Pinfall.new(['10']),
+      Pinfall.new(['9', '0']),
+      Pinfall.new(['7', '3']),
+      Pinfall.new(['4', '4']),
+      Pinfall.new(['10', '9', '0'])
+    ]
+  end
+
+  describe '#call' do
+    let(:result) do
+      "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10\r\n"\
+      "Jeff\r\n"\
+      "Pinfalls\t\tX\t7\t/\t9\t0\t\tX\t0\t8\t8\t/\tF\t6\t\tX\t\tX\tX\t8\t1\r\n"\
+      "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167\r\n"\
+      "John\r\n"\
+      "Pinfalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0\r\n"\
+      "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151"\
+    end
+
+    it { binding.pry; is_expected.to eq(result) }
+  end
+end
